826374cd3401386c9cd47b87cd0ff32fe27a00c4
seekplum <1131909224@qq.com>
2019-06-27 23:40:48 +0800

    MOD: 自动化部署openstack，更新部分接口

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,14 @@
 from __future__ import print_function
 
 import os
+import sys
 
 import textwrap
 
 from fabric.api import env, settings
 from fabric.api import run, cd
 from fabric.api import local, lcd
+from fabric.colors import red
 
 env.hosts = [host for host in os.environ.get("OPEN_STACK_IP", "").split(",") if host]
 # 配置SSH信息
@@ -37,9 +39,10 @@ class _OpenStackDeploy(_Singleton):
     """
 
     def __init__(self, run_command, run_cd):
-        self.run_command = run_command  # 执行系统命令函数
-        self.run_cd = run_cd  # 进入系统目录函数
+        self._run_command = run_command  # 执行系统命令函数
+        self._run_cd = run_cd  # 进入系统目录函数
 
+        self._os_type = "Ubuntu"
         self._stack_user = "stack"
         self._stack_home = "/opt/stack"
         self._stack_branch = "stable/ocata"
@@ -57,31 +60,34 @@ class _OpenStackDeploy(_Singleton):
             run_command, run_cd = local, lcd
         return cls(run_command, run_cd)
 
-    def run_stack_command(self, cmd):
+    def _run_stack_command(self, cmd):
         """进入stack用户执行命令
         """
         stack_cmd = 'su - %s -c "%s"' % (self._stack_user, cmd)
-        return self.run_command(stack_cmd)
+        return self._run_command(stack_cmd)
 
     def _check_os_type(self):
-        return self.run_command("uname -s")
+        return self._run_command("uname -v")
 
     def _check_os_version(self):
-        return self.run_command("cat /etc/issue")
+        return self._run_command("cat /etc/issue")
 
     def _check_os_memory(self):
-        return self.run_command("free -g")
+        return self._run_command("free -g")
 
     def _check_os_disk(self):
-        return self.run_command("df -Th")
+        return self._run_command("df -Th")
 
     def check(self):
-        self._check_os_type()
+        os_type = self._check_os_type()
+        if self._os_type not in os_type:
+            print(red("The operating system must be %s" % self._os_type))
+            sys.exit(1)
         self._check_os_version()
         self._check_os_memory()
         self._check_os_disk()
 
-    def backup_source(self):
+    def _backup_source(self):
         cmd = textwrap.dedent("""\
         test -f /etc/apt/sources.list && mv /etc/apt/sources.list /etc/apt/sources.list.$(date +%s).bak
         cat >/etc/apt/sources.list <<EOF
@@ -95,43 +101,43 @@ class _OpenStackDeploy(_Singleton):
         deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
         EOF
         """)
-        self.run_command(cmd)
+        self._run_command(cmd)
 
-    def update_soft(self):
-        self.run_command("sudo apt -y autoremove")
-        self.run_command("sudo apt-get -y update")
+    def _update_soft(self):
+        self._run_command("sudo apt -y autoremove")
+        self._run_command("sudo apt-get -y update")
 
-    def sync_time(self):
+    def _sync_time(self):
         cmd_list = [
             'sudo apt-get install -y ntpdate',
             'sudo ntpdate cn.pool.ntp.org',
             'date +"%F %T"',
         ]
         for cmd in cmd_list:
-            self.run_command(cmd)
+            self._run_command(cmd)
 
-    def create_user(self):
+    def _create_user(self):
         with settings(warn_only=True):
             cmd = 'id %s >/dev/null 2&> 1 || sudo useradd -s /bin/bash -d %s -m %s' % (
                 self._stack_user, self._stack_home, self._stack_user)
-            self.run_command(cmd)
+            self._run_command(cmd)
 
-        self.run_command('echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack')
+        self._run_command('echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack')
 
-    def upgrade_pip(self):
+    def _upgrade_pip(self):
         cmd = "pip install --upgrade pip"
-        self.run_command(cmd)
+        self._run_command(cmd)
 
-    def clone_devstack(self):
+    def _clone_devstack(self):
         cmd = "test -f %s || git clone -b %s --depth=1 %s %s" % (
             self._stack_root, self._stack_branch, self._stack_origin, self._stack_root)
-        self.run_stack_command(cmd)
+        self._run_stack_command(cmd)
 
-    def update_local_conf(self):
+    def _update_local_conf(self):
         sample_conf = os.path.join(self._stack_root, "samples/local.conf")
         local_conf = os.path.join(self._stack_root, "local.conf")
         cmd = "scp %s %s" % (sample_conf, local_conf)
-        self.run_stack_command(cmd)
+        self._run_stack_command(cmd)
 
         cmd = textwrap.dedent("""\
         cat >>/opt/stack/devstack/local.conf<<EOF
@@ -144,21 +150,21 @@ class _OpenStackDeploy(_Singleton):
         SPICE_REPO=http://git.trystack.cn/git/spice/spice-html5.git
         EOF
         """)
-        self.run_stack_command(cmd)
+        self._run_stack_command(cmd)
 
     def _update_package_version(self):
         cmd = 'sudo sed -i "s/^oslo.i18n==.*/oslo.i18n>=3.15.3/g" `grep ^oslo.i18n.* -rl %s`' % self._stack_home
-        self.run_command(cmd)
+        self._run_command(cmd)
 
         cmd = 'sudo sed -i "s/^oslo.utils==.*/oslo.utils>=3.33.0/g" `grep ^oslo.utils.* -rl %s`' % self._stack_home
-        self.run_command(cmd)
+        self._run_command(cmd)
 
         cmd = 'sudo sed -i "s/^pbr==.*/pbr!=2.1.0,>=2.0.0/g" `grep ^pbr.*.* -rl %s`' % self._stack_home
-        self.run_command(cmd)
+        self._run_command(cmd)
 
     def install(self):
         cmd = "cd %s && ./stack.sh" % self._stack_root
-        self.run_stack_command(cmd)
+        self._run_stack_command(cmd)
 
     def uninstall(self):
         """卸载操作
@@ -168,11 +174,11 @@ class _OpenStackDeploy(_Singleton):
         3.卸载相关Python三方包
         """
         with settings(warn_only=True):
-            self.run_stack_command("cd %s && ./unstack.sh" % self._stack_root)
-            self.run_stack_command("cd %s && ./clean.sh" % self._stack_root)
+            self._run_stack_command("cd %s && ./unstack.sh" % self._stack_root)
+            self._run_stack_command("cd %s && ./clean.sh" % self._stack_root)
 
             cmd = "userdel -rf %(user)s" % dict(user=self._stack_user)
-            self.run_command(cmd)
+            self._run_command(cmd)
 
             ignore_set = {
                 "python-apt",
@@ -180,70 +186,94 @@ class _OpenStackDeploy(_Singleton):
                 "pycrypto",
             }
             cmd = 'pip freeze | grep -E -v "%s" | xargs pip uninstall -y' % "|".join(ignore_set)
-            self.run_command(cmd)
+            self._run_command(cmd)
 
-    def update_pip_conf(self):
+    def _update_pip_conf(self):
         cmd = "mkdir -p ~/.pip/"
-        self.run_command(cmd)
+        self._run_command(cmd)
 
         cmd = textwrap.dedent("""\
         cat >~/.pip/pip.conf<<EOF
         [global]
         index-url = https://pypi.tuna.tsinghua.edu.cn/simple
         extra-index-url = https://pypi.org/simple
-        timeout = 120
+        timeout = 300
         trusted-host =
             pypi.tuna.tsinghua.edu.cn
             pypi.org
         EOF
         """)
-        self.run_command(cmd)
+        self._run_command(cmd)
 
-    def disable_upgrade_pip(self):
+    def _disable_upgrade_pip(self):
         cmd = 'sed -in "s/^install_get_pip/#install_get_pip/g" %s' % os.path.join(self._stack_root,
                                                                                   "tools/install_pip.sh")
-        self.run_command(cmd)
+        self._run_command(cmd)
+
+    def update(self):
+        self._backup_source()
+        self._sync_time()
+        self._update_soft()
+        self._update_pip_conf()
+        self._upgrade_pip()
+
+    def prepare(self):
+        self._create_user()
+        self._clone_devstack()
+        self._disable_upgrade_pip()
+        self._update_local_conf()
 
     def deploy(self):
-        self.uninstall()
         self.check()
-        self.backup_source()
-        self.update_soft()
-        self.update_pip_conf()
-        self.upgrade_pip()
-
-        self.create_user()
-        self.clone_devstack()
-        self.disable_upgrade_pip()
-        self.update_local_conf()
+        self.uninstall()
+        self.update()
+        self.prepare()
         self.install()
 
 
 def check():
-    """检查系统内存、磁盘空间
+    """检查系统内存、磁盘空间等
     """
     d = _OpenStackDeploy.get_deploy()
     d.check()
 
 
+def update():
+    """更新配置和软件
+    """
+    d = _OpenStackDeploy.get_deploy()
+    d.check()
+    d.update()
+
+
+def prepare():
+    """准备安装环境
+    """
+    d = _OpenStackDeploy.get_deploy()
+    d.check()
+    d.prepare()
+
+
 def deploy():
-    """执行部署操作，部署前会进行清理环境操作
+    """执行部署操作(部署前会进行清理环境、更新配置等操作)
     """
     d = _OpenStackDeploy.get_deploy()
     d.deploy()
 
 
 def install():
-    """只执行安装操作，不会进行卸载清理环境等
+    """执行安装操作(不进行卸载清理环境、更新配置等操作)
     """
     d = _OpenStackDeploy.get_deploy()
+    d.check()
     d.install()
 
 
 def uninstall():
-    """卸载
+    """卸载OpenStack
     """
     d = _OpenStackDeploy.get_deploy()
+    d.check()
     d.uninstall()
 
 # if __name__ == '__main__':
