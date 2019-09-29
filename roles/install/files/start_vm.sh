973133f0fa99a0476b45942248edef8555a90dc2
seekplum <1131909224@qq.com>
2019-09-29 18:59:12 +0800

    MOD: 修改VirtualBox启动脚本

--- roles/install/files/start_vm.sh
+++ roles/install/files/start_vm.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-for host in $(VBoxManage list vms | grep -E "ubuntu|centos" | awk '{print $1}' | sed 's/.$//' | sed 's/^.//'); do VBoxManage startvm ${host} --type headless; done
+vms_txt='/tmp/.vms_temp.txt'
+runningvms_txt='/tmp/.runningvms_temp.txt'
+
+VBoxManage list vms | grep "ubuntu" | awk '{print $1}' | sed 's/.$//' | sed 's/^.//' > ${vms_txt}
+VBoxManage list runningvms | awk '{print $1}' | awk '{gsub(/^"|"$/, "");print}' > ${runningvms_txt}
+
+stophosts=$(sort -m <(sort ${vms_txt} | uniq) <(sort ${runningvms_txt} | uniq) <(sort ${runningvms_txt} | uniq) | uniq -u)
+for host in ${stophosts}; do VBoxManage startvm ${host} --type headless; done
+
+rm -f ${vms_txt}
+rm -f ${runningvms_txt}
+
--- roles/install/files/stop_vm.sh
+++ roles/install/files/stop_vm.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for host in $(VBoxManage list runningvms | awk '{print $1}' | awk '{gsub(/^"|"$/, "");print}'); do VBoxManage controlvm ${host} poweroff; done
