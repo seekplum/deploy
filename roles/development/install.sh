6b9617dbcc3b83d857c7eb03433086815f074c58
seekplum <1131909224@qq.com>
2019-12-25 21:29:06 +0800

    ADD: gerrit, ldap打通

---
+++ roles/development/install.sh
@@ -0,0 +1,79 @@
+docker stop ldap ldapadmin gerrit > /dev/null 2>&1 || echo "stop container"
+docker rm ldap ldapadmin gerrit > /dev/null 2>&1 || echo "delete container"
+
+export LDAP_SERVER_IP=$(ifconfig | grep "inet " | grep -v "127.0.0.1" | grep -v "172." | grep -v "10.244" | awk '{print $2}' | cut -d":" -f 2)
+export VOLUMES_ROOT="/tmp/data"
+
+rm -rf ${VOLUMES_ROOT}
+
+
+docker run -d  \
+    -p 389:389  \
+    -p 636:636  \
+    --network bridge  \
+    --hostname openldap-host  \
+    -v ${VOLUMES_ROOT}/slapd/database:/var/lib/ldap  \
+    -v ${VOLUMES_ROOT}/slapd/config:/etc/ldap/slapd.d  \
+    -e LDAP_ORGANISATION='seekplum.io'  \
+    -e LDAP_DOMAIN='seekplum.io'  \
+    -e LDAP_ADMIN_PASSWORD='seekplum'  \
+    -e LDAP_TLS='false'  \
+    -e LDAP_READONLY_USER='true'  \
+    -e LDAP_READONLY_USER_USERNAME='guest'  \
+    -e LDAP_READONLY_USER_PASSWORD='123456'  \
+    --name ldap  \
+    osixia/openldap:1.1.9
+
+sleep 2
+
+# 创建用户组
+ldapadd -c -h ${LDAP_SERVER_IP} -p 389 -w seekplum -D 'cn=admin,dc=seekplum,dc=io' -f users.ldif
+
+# 创建用户
+bash ldap.sh create zhangsan 123456 张三
+bash ldap.sh create lisi 123456 李四
+
+# 检查用户名密码是否正确
+ldapwhoami -h ${LDAP_SERVER_IP} -p 389 -D 'cn=admin,dc=seekplum,dc=io' -w seekplum
+ldapwhoami -h ${LDAP_SERVER_IP} -p 389 -D 'cn=guest,dc=seekplum,dc=io' -w 123456
+ldapwhoami -h ${LDAP_SERVER_IP} -p 389 -D 'cn=hjd,ou=users,dc=seekplum,dc=io' -w 123456
+ldapwhoami -h ${LDAP_SERVER_IP} -p 389 -D 'cn=zhangsan,ou=users,dc=seekplum,dc=io' -w 123456
+ldapwhoami -h ${LDAP_SERVER_IP} -p 389 -D 'cn=lisi,ou=users,dc=seekplum,dc=io' -w 123456
+
+docker run -d  \
+    --privileged  \
+    -p 8089:80  \
+    --link ldap:ldap  \
+    -e PHPLDAPADMIN_LDAP_HOSTS=ldap  \
+    -e PHPLDAPADMIN_HTTPS=false  \
+    --name ldapadmin  \
+    osixia/phpldapadmin
+    
+docker run -d \
+    --name gerrit \
+    -p 8088:8080 \
+    -p 29418:29418 \
+    -v ${VOLUMES_ROOT}/gerrit:/var/gerrit/review_site \
+    -e WEBURL=http://${LDAP_SERVER_IP}:8088 \
+    -e GITWEB_TYPE=gitiles \
+    -e AUTH_TYPE=LDAP \
+    -e  LDAP_SERVER=ldap://${LDAP_SERVER_IP} \
+    -e LDAP_ACCOUNTBASE='dc=seekplum,dc=io' \
+    -e LDAP_ACCOUNTPATTERN='(cn=${username})' \
+    -e LDAP_ACCOUNTSSHUSERNAME='${cn}' \
+    -e LDAP_ACCOUNTFULLNAME='${sn}' \
+    -e LDAP_USERNAME='cn=guest,dc=seekplum,dc=io' \
+    -e LDAP_PASSWORD='123456' \
+    -e GERRIT_INIT_ARGS='--install-plugin=download-commands' \
+    openfrontier/gerrit
+
+gerrit_code=0
+count=0
+while [ ${gerrit_code} -ne 200 ]
+    do
+        let "gerrit_code=$(curl -I -m 10 -o /dev/null -s -w %{http_code} http://${LDAP_SERVER_IP}:8088)"
+        sleep 1
+        let "count++"
+    done
+
+echo -e "\nhttp://${LDAP_SERVER_IP}:8088"
---
+++ roles/development/ldap.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+ETVAL=0
+
+# const
+# LDAP_SERVER_IP="127.0.0.1"
+LDAP_SERVER_PORT="389"
+LDAP_ADMIN_USER="cn=admin,dc=seekplum,dc=io"
+LDAP_ADMIN_PASS="seekplum"
+
+create_user () {
+    if [ x"$#" != x"3" ];then
+        echo "Usage: bash $0 create <username> <password> <realname>"
+        exit -1
+    fi
+
+    # param
+    USERNAME="$1"
+    PASSWORD="$2"
+    ENCRYPT_PASSWORD=$(slappasswd -h {ssha} -s "$PASSWORD")
+    REALNAME="$3"
+    REALNAME_BASE64=$(echo -n $REALNAME | base64)
+
+    # add count & group 
+    cat <<EOF | ldapmodify -c -h $LDAP_SERVER_IP -p $LDAP_SERVER_PORT -w $LDAP_ADMIN_PASS -D $LDAP_ADMIN_USER 
+dn: cn=$USERNAME,ou=users,dc=seekplum,dc=io
+changetype: add
+objectClass: top
+objectClass: person
+objectClass: organizationalPerson
+objectClass: inetOrgPerson
+cn: $USERNAME
+sn:: $REALNAME_BASE64
+mail: $USERNAME@qq.com
+userPassword: $ENCRYPT_PASSWORD
+
+dn: cn=Users,ou=groups,dc=seekplum,dc=io
+changetype: modify
+add: uniqueMember
+uniqueMember: cn=$USERNAME,ou=users,dc=seekplum,dc=io
+EOF
+}
+
+modify_password () {
+    if [ x"$#" != x"2" ];then
+        echo "Usage: bash $0 modify <username> <newPassword>"
+        exit -1
+    fi
+
+    # param
+    USERNAME="$1"
+    PASSWORD="$2"
+    ENCRYPT_PASSWORD=$(slappasswd -h {ssha} -s "$PASSWORD")
+
+    # modify
+    cat <<EOF | ldapmodify -c -h $LDAP_SERVER_IP -p $LDAP_SERVER_PORT -w $LDAP_ADMIN_PASS -D $LDAP_ADMIN_USER 
+dn: cn=$USERNAME,ou=users,dc=seekplum,dc=io
+changetype: modify
+replace: userPassword
+userPassword: $ENCRYPT_PASSWORD
+EOF
+}
+
+delete_user () {
+    if [ x"$#" != x"1" ];then
+        echo "Usage: bash $0 delete <username>"
+        exit -1
+    fi
+
+    # param
+    USERNAME="$1"
+
+    # delete user 
+    ldapdelete -c -h $LDAP_SERVER_IP -p $LDAP_SERVER_PORT -w $LDAP_ADMIN_PASS -D $LDAP_ADMIN_USER "cn=$USERNAME,ou=users,dc=seekplum,dc=io"
+}
+
+print_help() {
+        echo "Usage: bash $0 {create|modify|delete}"
+    echo "e.g: $0 create"
+}
+
+# 命令行参数小于 1 时打印提示信息后退出
+if [ $# -lt 1 ] ; then
+    print_help
+    exit 1;
+fi
+
+case "$1" in
+  create)
+        create_user ${@:2}
+        ;;
+  delete)
+        delete_user ${@:2}
+        ;;
+  modify)
+        modify_password ${@:2}
+        ;;
+  "")
+  # -h|--help)
+        print_help  # 参数为空时执行
+        ETVAL=1
+        ;;
+  *)  # 匹配都失败执行
+        print_help
+        ETVAL=1
+esac
+
+exit ${ETVAL}
