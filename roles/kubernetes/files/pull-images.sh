fac77853068092404970a12e8fe14cc1e141061c
seekplum <1131909224@qq.com>
2020-03-30 23:12:45 +0800

    MOD: 下载k8s镜像脚本自动适配版本

--- roles/kubernetes/files/pull-images.sh
+++ roles/kubernetes/files/pull-images.sh
@@ -1,18 +1,9 @@
 #/usr/bin/env bash
 
-# kubeadm config images list 
-
-images=(
-    kube-apiserver:v1.16.7
-    kube-controller-manager:v1.16.7
-    kube-scheduler:v1.16.7
-    kube-proxy:v1.16.7
-    pause:3.1
-    etcd:3.3.15-0
-    coredns:1.6.2
-)
-for imageName in ${images[@]} ; do
-    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
-    docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
-    docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
+for imageName in $(kubeadm config images list 2> /dev/null); do
+    # name=$(echo ${imageName} | cut -d"/" -f2)
+    name=${imageName#*/}
+    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/${name}
+    docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/${name} k8s.gcr.io/${name}
+    docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/${name}
 done
