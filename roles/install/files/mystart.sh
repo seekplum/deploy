174181c950fca4b2cfebc32ee2b6ed454f6f0a0e
seekplum <1131909224@qq.com>
2021-04-13 22:25:59 +0800

    ADD: 增加开机启动脚本

---
+++ roles/install/files/mystart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+# 增加 ssh key 到可信列表
+ssh-add -K ~/.ssh/seekplum
+
+echo "$(date +'%Y-%m-%d %H:%M:%S')  ${HOME}/packages/pythonenv/3.7.5/bin/supervisord -c ${HOME}/packages/supervisor/supervisord.conf" >> ~/packages/mystart.log
+
+# 启动相关服务
+${HOME}/packages/pythonenv/3.7.5/bin/supervisord -c ${HOME}/packages/supervisor/supervisord.conf
+
+# 开启第二个微信页面
+# screen -S weixin /Applications/WeChat.app/Contents/MacOS/WeChat
