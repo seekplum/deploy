e6461c190557a7bccdfc12850bbc6fff7a93fc36
seekplum <1131909224@qq.com>
2023-04-07 14:42:59 +0800

    MOD: centos 增加开机脚本

--- roles/install/templates/mystart.sh
+++ roles/install/templates/mystart.sh
@@ -3,12 +3,13 @@
 set -xe
 
 # 增加 ssh key 到可信列表
-ssh-add -K ~/.ssh/seekplum
-
-echo "$(date +'%Y-%m-%d %H:%M:%S') ${HOME}/packages/pythonenv/{{PYTHON3_VERSION}}/bin/supervisord -c ${HOME}/packages/supervisor/supervisord.conf" >> ~/packages/mystart.log
+# ssh-add -K ~/.ssh/seekplum >/dev/null 2&> 1
 
 # 启动相关服务
 ${HOME}/packages/pythonenv/{{PYTHON3_VERSION}}/bin/supervisord -c ${HOME}/packages/supervisor/supervisord.conf
 
 # 开启第二个微信页面
 # screen -S weixin /Applications/WeChat.app/Contents/MacOS/WeChat
+
+# mount -o remount,rw /
+# systemctl daemon-reload && systemctl restart systemd-resolved
