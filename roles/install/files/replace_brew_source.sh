38814fb8e99fa6cfd0f353182dd7122f026792d8
seekplum <1131909224@qq.com>
2026-05-22 20:31:06 +0800

    FIX: 修复 act/drone 安装失败

--- roles/install/files/replace_brew_source.sh
+++ roles/install/files/replace_brew_source.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 # 替换 Homebrew 源为清华镜像，支持重复执行
 
-set -e
+set -ex
 
 BREW_REPO="$(brew --repo)"
 BREW_CORE_REPO="$(brew --repo homebrew/core)"
 BREW_CASK_REPO="$(brew --repo homebrew/cask)"
 
-TSINGHUA_BREW="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
-TSINGHUA_CORE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
-TSINGHUA_CASK="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git"
-TSINGHUA_BOTTLE="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 
 git_cmd() {
     sudo git "${@}"
@@ -22,8 +18,8 @@ replace_origin() {
 
     if [ -d "$repo_path" ]; then
         echo "$repo_path"
-        git_cmd remote -v && git_cmd branch
         cd "$repo_path"
+        git_cmd remote -v && git_cmd branch
         if git_cmd remote get-url origin >/dev/null 2>&1; then
             git_cmd remote set-url origin "$remote_url"
         else
@@ -38,8 +34,49 @@ replace_origin() {
     fi
 }
 
-replace_origin "$BREW_REPO" "$TSINGHUA_BREW"
-replace_origin "$BREW_CORE_REPO" "$TSINGHUA_CORE"
-replace_origin "$BREW_CASK_REPO" "$TSINGHUA_CASK"
+function replace_tsinghua() {
+    replace_origin "$BREW_REPO" "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+    replace_origin "$BREW_CORE_REPO" "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+    replace_origin "$BREW_CASK_REPO" "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git"
 
-brew update
+    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+}
+
+function replace_ustc() {
+    replace_origin "$BREW_REPO" "https://mirrors.ustc.edu.cn/brew.git"
+    replace_origin "$BREW_CORE_REPO" "https://mirrors.ustc.edu.cn/homebrew-core.git"
+    replace_origin "$BREW_CASK_REPO" "https://mirrors.ustc.edu.cn/homebrew-cask.git"
+
+    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+    export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
+}
+
+function replace_official() {
+    replace_origin "$BREW_REPO" "https://github.com/Homebrew/brew.git"
+    replace_origin "$BREW_CORE_REPO" "https://github.com/Homebrew/homebrew-core.git"
+    replace_origin "$BREW_CASK_REPO" "https://github.com/Homebrew/homebrew-cask.git"
+}
+
+function print_help() {
+    echo "Usage: bash $0 {official|tsinghua|ustc}"
+    echo "e.g: bash $0 ustc"
+}
+
+case "$1" in
+    official)
+        replace_official
+        ;;
+    tsinghua)
+        replace_tsinghua
+        ;;
+    "" | ustc)
+        replace_ustc
+        ;;
+    -h | --help)
+        print_help
+        ;;
+    *) # 匹配都失败执行
+        print_help
+        ;;
+esac
+
--- roles/install/templates/.bash_profile
+++ roles/install/templates/.bash_profile
@@ -68,7 +68,8 @@ export DIRENV_LOG_FORMAT=""
 
 {% if is_mac_os %}
 export PS1="\h@\u: \W \$ "              # 终端提示符
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
 {% else %}
 export PS1="\[\e[0;33m\]\u\[\e[0m\]@\[\e[0;37m\]\h\[\e[0m\]:\[\e[0;31m\] \w \[\e[0m\]\$ " # 终端提示符
 {% endif %}
